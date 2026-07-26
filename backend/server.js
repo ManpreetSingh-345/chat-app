42e25404a54f86fbe09c9d041be142c191873010
Manpreet Singh <manpreet345@yahoo.com>
2026-07-26 14:45:17 -0400

    Add testing logic for Socket IO

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,27 @@
+import dotenv from "dotenv";
 import express from "express";
+import { Server } from "socket.io";
+import { createServer } from "node:http";
 
+dotenv.config();
 const app = express();
+const server = createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: ["http://localhost:5173"],
+  },
+});
+
 app.get("/", (req, res) => {
   console.log("/ route reached");
   res.status(200).send({ message: "Successful API call" });
 });
 
-app.listen(8000, () => {
-  console.log("App listening on port 8000");
+io.on("connection", (socket) => {
+  console.log("Client connected successfully");
+});
+
+server.listen(process.env.PORT, () => {
+  console.log(`App listening on port ${process.env.PORT}`);
 });
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
+import { io } from "socket.io-client";
 
 function App() {
+  const socket = io("http://localhost:3000");
   return <h1>Hello, world</h1>;
 }
 
